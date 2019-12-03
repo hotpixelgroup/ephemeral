6ed8d07cbc3ec10d5c617936c20508d783734261
Harry Beckwith <harry@hotpixelgroup.com>
2019-12-03 15:50:48 -0600

    testing with Jon and Jessie

--- apps/ephemeral/pages/index.tsx
+++ apps/ephemeral/pages/index.tsx
@@ -27,49 +27,50 @@ export const Index = () => {
             If you like Nx, please give it a star:
             <div className="github-star-badge">
               <svg
-                className="material-icons"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-              </svg>
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
+                          className="material-icons"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M0 0h24v24H0z" fill="none" />
+                          <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                        </svg>
+                        Star
+                      </div>
+                    </a>
+                  </div>
+                  <p>Here is something else</p>
+                  <p>Here are some links to help you get started.</p>
+                  <ul className="resources">
+                    <li className="col-span-2">
+                      <a
+                        className="resource flex"
+                        href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
+                                      >
+                                        Nx video course
+                                      </a>
+                                    </li>
+                                    <li className="col-span-2">
+                                      <a
+                              className="resource flex"
+                              href="https://nx.dev/react/getting-started/what-is-nx"
+                            >
+                              Nx video tutorial
+                            </a>
+                          </li>
+                          <li className="col-span-2">
             <a
               className="resource flex"
-              href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
-            >
-              Nx video course
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/getting-started/what-is-nx"
-            >
-              Nx video tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://connect.nrwl.io/">
-              <img
-                height="36"
+                      href="https://nx.dev/react/tutorial/01-create-application"
+                    >
+                      Interactive tutorial
+                    </a>
+                  </li>
+                  <li className="col-span-2">
+                    <a className="resource flex" href="https://connect.nrwl.io/">
+                      <img
+                        height="36"
                 alt="Nrwl Connect"
                 src="https://connect.nrwl.io/assets/img/CONNECT_ColorIcon.png"
               />
