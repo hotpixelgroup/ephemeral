356dc510bc0b70a7f7f2688d7e223d8e0a01a180
Harry Beckwith <harry@hotpixelgroup.com>
2019-12-03 14:58:16 -0600

    testing linter hook

--- apps/ephemeral/pages/index.tsx
+++ apps/ephemeral/pages/index.tsx
@@ -26,23 +26,23 @@ export const Index = () => {
             {' '}
             If you like Nx, please give it a star:
             <div className="github-star-badge">
-              <svg
-                className="material-icons"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-              </svg>
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
+                                  <svg
+                                    className="material-icons"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                                      viewBox="0 0 24 24"
+                                                    >
+                                                      <path d="M0 0h24v24H0z" fill="none" />
+                                                      <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                                                    </svg>
+                                  Star
+                                </div>
+                              </a>
+                            </div>
+                            <p>Here are some links to help you get started.</p>
+                            <ul className="resources">
+                              <li className="col-span-2">
             <a
               className="resource flex"
               href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
